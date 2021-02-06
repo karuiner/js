7a1acddeee0fad1dda88826b27683bbd0c83f57f
karuiner <intaekgong@gmail.com>
2021-02-06 17:19:03 +0900

    ㄴㅁㅇ

---
+++ test_js/timeinterval.js
@@ -0,0 +1,13 @@
+let f = function (s) {
+    console.log(s);
+};
+
+//setInterval(f, 1000);
+// setTimeout(
+//     function run(i) {
+//         console.log(i);
+//         return i < 100 ? setTimeout(run, 5000, i + 1) : i;
+//     },
+//     5000,
+//     0
+// );
