30095d7a60938cef044d6715a9ed9ddb63823abb
karuiner <intaekgong@gmail.com>
2021-02-14 16:11:16 +0900

    test

--- test/firework/index.js
+++ test/firework/index.js
@@ -8,6 +8,7 @@ let present,
     a0 = 4,
     mote_list = [];
 color_set = ["red", "green", "yellow", "purple", "blue"];
+
 function mk_ball() {
     let ball = document.createElement("div");
     ball.classList.add("ball");
@@ -17,6 +18,56 @@ function mk_ball() {
     pa.append(ball);
     return ball;
 }
+// 기본 자료를 입력 받고 실행함수를 돌려주는 외장함수
+function input_fire(target, stime) {
+    let vx0 = 200,
+        vy0 = -12;
+
+    return function (ptime) {
+        let t = (ptime - stime) / 1000,
+            nx = vx0 * t,
+            ny = vy0 * t + 0.5 * a0 * t ** 2;
+        if (t < 3) {
+            if (target.style.transition === "") {
+                target.style.transition = "All 0.05s";
+            }
+            target.style.transform = `translate(${nx}px,${ny}vh )`;
+        } else {
+            target.style.background = "black";
+            //            pa.removeChild(target);
+            //            return "end";
+            return make_inner_bomb(target, ptime);
+        }
+    };
+}
+
+function make_inner_bomb(pnode, stime) {
+    let bomb_flag = [];
+
+    for (let i = 0; i < 200; i++) {
+        let target = mk_mote(pnode),
+            func;
+        let v = 20,
+            theta = Math.PI * Math.random(),
+            phi = 2 * Math.PI * Math.random();
+        if (i === 0) {
+            func = input_motion_in_gravity(target, stime, v, theta, phi, true);
+        } else {
+            func = input_motion_in_gravity(target, stime, v, theta, phi);
+        }
+        bomb_flag.push(func);
+    }
+    return function (ptime) {
+        bomb_flag = bomb_flag.filter(function (x) {
+            y = x(ptime);
+            return y !== "end";
+        });
+        if (bomb_flag.length === 0) {
+            return "end";
+        }
+    };
+}
+
 function fire(arr) {
     [target, time] = arr;
     // 필요한것  위치 계산 인자
@@ -44,21 +95,60 @@ function fire(arr) {
     }
 }
 
+function fire2(target, t = 0) {
+    let vx0 = 200,
+        vy0 = -10,
+        nx = vx0 * t,
+        ny = vy0 * t + 0.5 * a0 * t ** 2;
+    target.style.transform = `translate(${nx}px,${ny}vh )`;
+    if (target.style.transition === "") {
+        target.style.transition = "All 0.01s";
+    }
+    if (t > 6) {
+        pa.removeChild(target);
+    } else {
+        setTimeout(fire2, 20, target, t + 0.02);
+    }
+}
+
 function color_pick() {
     let z = Math.random() * 5;
     z = z > 4 ? 4 : parseInt(z);
     return color_set[z];
 }
 
-function mk_mote() {
+function mk_mote(pnode = pa) {
     let mote = document.createElement("div");
     mote.classList.add("mote");
     mote.style.position = "fixed";
-    mote.style.top = "14vh";
-    mote.style.left = "50vw";
     mote.style.background = color_pick();
-    pa.append(mote);
+    pnode.append(mote);
     return mote;
+}
+
+function input_motion_in_gravity(target, stime, v, theta, phi, check = false) {
+    let vset = toxyz(v, theta, phi);
+    return function (time) {
+        let t = (time - stime) / 1000,
+            x = vset[1] * t,
+            y = vset[2] * t + 0.5 * a0 * t ** 2;
+
+        if (t < 4) {
+            if (target.style.transition === "") {
+                target.style.transition = "All 0.05s";
+            }
+            target.style.opacity = 0.2 + Math.random() * 0.7;
+            target.style.transform = `translate(${x}px,${y}px )`;
+        } else if (t < 8) {
+            target.style.opacity *= 0.7;
+            target.style.transform = `translate(${x}px,${y}px )`;
+        } else {
+            if (check) {
+                target.parentElement.parentElement.removeChild(target.parentElement);
+            }
+            return "end";
+        }
+    };
 }
 
 function motion_in_gravity(target, stime, time, st, v, theta, phi) {
@@ -128,17 +218,22 @@ function frame(timestamp) {
     present = timestamp;
 
     // 대포알 발사 예제 1개씩 무한히
-    // if (pa.childElementCount < 1) {
-    //     let target = mk_ball();
-    //     target_list.push([target, present]);
-    // }
-
-    // for (let i of target_list) {
-    //     fire(i);
-    // }
+    if (pa.childElementCount < 1) {
+        let target = mk_ball();
+        target_list.push(input_fire(target, present));
+    }
+    target_list = target_list.map(function (x) {
+        y = x(present);
+        if (typeof y === "function") {
+            return y;
+        } else if (y !== "end") {
+            return x;
+        }
+    });
+    target_list = target_list.filter((x) => typeof x === "function");
 
     // 중심에서 불꽃을 원형으로 던져내는 예제를 만들어야한다.
-    fire_thrower();
+    // fire_thrower();
 
     window.requestAnimationFrame(frame);
 }
