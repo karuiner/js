b8e020ca936265ac6ee73bf72d534d0e806e2f59
karuiner <intaekgong@gmail.com>
2021-03-11 09:34:48 +0900

    update

--- practice_coding/leetcord/easy/Count_and_Say.js
+++ practice_coding/leetcord/easy/Count_and_Say.js
@@ -35,4 +35,30 @@ var countAndSay = function (n) {
     }
     return ans;
 };
+//Runtime: 80 ms, faster than 87.23% of JavaScript online submissions for Count and Say.
+//Memory Usage: 40.2 MB, less than 88.47% of JavaScript online submissions for Count and Say.
 // 재귀함수용 인것 같다. 재귀함수 형식으로 바꿔서 만들어 보도록 하자
+var countAndSay = function (n, str = "") {
+    if (str === "" && n > 0) {
+        str = countAndSay(n - 1, "1");
+    } else if (n > 0) {
+        let test = "",
+            i = str[0],
+            j = 0;
+        while (str !== "") {
+            if (i === str[0]) {
+                j += 1;
+            } else {
+                test += `${j}${i}`;
+                i = str[0];
+                j = 1;
+            }
+            str = str.slice(1);
+        }
+        test += `${j}${i}`;
+        str = countAndSay(n - 1, test);
+    }
+    return str;
+};
+//Runtime: 88 ms, faster than 49.07% of JavaScript online submissions for Count and Say.
+//Memory Usage: 42.3 MB, less than 9.97% of JavaScript online submissions for Count and Say.
