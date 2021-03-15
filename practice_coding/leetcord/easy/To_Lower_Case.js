c3173ba2ebeaa8552d64a8e6fd0aabb4131c191a
karuiner <intaekgong@gmail.com>
2021-03-15 17:32:49 +0900

    update

---
+++ practice_coding/leetcord/easy/To_Lower_Case.js
@@ -0,0 +1,10 @@
+/*
+ *
+ * @param {string} str
+ * @return {string}
+ */
+var toLowerCase = function (str) {
+    return str.toLowerCase();
+};
+//Runtime: 68 ms, faster than 96.96% of JavaScript online submissions for To Lower Case.
+//Memory Usage: 38.5 MB, less than 37.59% of JavaScript online submissions for To Lower Case.
