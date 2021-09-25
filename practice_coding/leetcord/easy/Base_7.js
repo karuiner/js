2f1676b660851cf66071bbc95b2e424af20afa33
karuiner <intaekgong@gmail.com>
2021-09-25 22:57:54 +0900

    leetcord add easy

---
+++ practice_coding/leetcord/easy/Base_7.js
@@ -0,0 +1,11 @@
+/*
+ *
+ * @param {number} num
+ * @return {string}
+ */
+var convertToBase7 = function (num) {
+  return num.toString(7);
+};
+
+// Runtime: 82 ms, faster than 29.38% of JavaScript online submissions for Base 7.
+// Memory Usage: 40 MB, less than 11.86% of JavaScript online submissions for Base 7.
