4eac5c2b238b46a7909596eb26bfd14e18311eaa
karuiner <intaekgong@gmail.com>
2021-10-24 23:16:35 +0900

    리트코드 결과 추가

--- practice_coding/leetcode/medium/My_Calendar_I.js
+++ practice_coding/leetcode/medium/My_Calendar_I.js
@@ -34,3 +34,6 @@ MyCalendar.prototype.book = function (start, end) {
  * var obj = new MyCalendar()
  * var param_1 = obj.book(start,end)
  */
+
+// Runtime: 306 ms, faster than 39.00% of JavaScript online submissions for My Calendar I.
+// Memory Usage: 48.9 MB, less than 23.00% of JavaScript online submissions for My Calendar I.
