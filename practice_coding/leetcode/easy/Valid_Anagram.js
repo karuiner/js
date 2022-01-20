2f94fd96c7d0bf8814bc5c89bbb7b357430d3a1e
karuiner <intaekgong@gmail.com>
2022-01-20 22:16:50 +0900

    leetcode add easy (242. Valid Anagram)

---
+++ practice_coding/leetcode/easy/Valid_Anagram.js
@@ -0,0 +1,37 @@
+/*
+ *
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+  let db = {},
+    n = s.length,
+    n2 = t.length;
+  if (n !== n2) {
+    return false;
+  }
+  for (let i = 0; i < n; i++) {
+    if (db[s[i]] === undefined) {
+      db[s[i]] = 1;
+    } else {
+      db[s[i]]++;
+    }
+  }
+  for (let i = 0; i < n2; i++) {
+    if (db[t[i]] === undefined) {
+      db[t[i]] = -1;
+    } else {
+      db[t[i]]--;
+    }
+  }
+  let ans = 0;
+  for (let i in db) {
+    ans += Math.abs(db[i]);
+  }
+
+  return ans === 0;
+};
+
+// Runtime: 88 ms, faster than 85.38% of JavaScript online submissions for Valid Anagram.
+// Memory Usage: 40.9 MB, less than 69.21% of JavaScript online submissions for Valid Anagram.
