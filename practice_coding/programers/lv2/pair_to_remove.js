ac7668ba7dcefaea8e473d2a8e68e1db2c1164e8
karuiner <intaekgong@gmail.com>
2022-02-23 22:52:22 +0900

    프로그래머스  level 2 푸는 중 (짝지어 제거하기)

--- practice_coding/programers/lv2/pair_to_remove.js
+++ practice_coding/programers/lv2/pair_to_remove.js
@@ -1,28 +1,49 @@
 // 짝지어 제거하기
+
+// stack 이라는 힌트를 보고 생각해본것
+// 하지만 효율성 부분에서 오래걸림.(이전 방식에 비해서는 훨씬 효율적임.)
 function solution(s) {
-  var ans = 1,
-    n = s.length;
+  let n = s.length;
   if (n % 2 !== 0) {
     return 0;
   }
-  let db = {};
+  let ds = "";
   for (let i = 0; i < n; i++) {
-    if (db[s[i]] === undefined) {
-      db[s[i]] = [i];
-    } else {
-      let k = i - db[s[i]][0];
-      if (k === 1) {
-        delete db[s[i]];
-      } else if (k % 2 === 1) {
-        db[s[i]] = [i, ...db[s[i]]];
-      } else {
-        ans = 0;
-        break;
+    ds = s[i] + ds;
+    if (ds.length > 1) {
+      if (ds[0] === ds[1]) {
+        ds = ds.slice(2);
       }
     }
   }
-  return ans;
+  return ds.length === 0 ? 1 : 0;
 }
+
+// 미완성
+// function solution(s) {
+//   var ans = 1,
+//     n = s.length;
+//   if (n % 2 !== 0) {
+//     return 0;
+//   }
+//   let db = {};
+//   for (let i = 0; i < n; i++) {
+//     if (db[s[i]] === undefined) {
+//       db[s[i]] = [i];
+//     } else {
+//       let k = i - db[s[i]][0];
+//       if (k === 1) {
+//         delete db[s[i]];
+//       } else if (k % 2 === 1) {
+//         db[s[i]] = [i, ...db[s[i]]];
+//       } else {
+//         ans = 0;
+//         break;
+//       }
+//     }
+//   }
+//   return ans;
+// }
 
 // 정확한 값을 돌려주는 듯하지만 속도가 느리다.
 // function solution(s) {
