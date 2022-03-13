6630f44e954cf09b287b41199b887de8cecec6f3
karuiner <intaekgong@gmail.com>
2022-03-13 21:37:55 +0900

    프로그래머스 거리두기 확인하기(Level 2) 풀이완료

--- practice_coding/programers/lv2/check_the_distance.js
+++ practice_coding/programers/lv2/check_the_distance.js
@@ -1,106 +1,166 @@
 //거리두기 확인하기
-// 풀이 완료
-
-//2차 시도 [정확성: 94.0, 합계: 94.0 / 100.0]
+// 풀이 완료 [정확성: 100.0, 합계: 100.0 / 100.0]
 function solution(places) {
-  function r(a, b) {
-    return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
-  }
-
   function check(arr) {
-    let p = [],
-      c = 0,
-      ans = 1;
-    function IsOpen(a, b) {
-      let [i1, j1] = a,
-        [i2, j2] = b,
-        ans = true,
-        ans1 = true;
-      let k1 = i2 - i1;
-      function gotoi(i1, i2, j) {
-        let ans = true;
-        if (i1 > i2) {
-          for (let i = i1; i >= i2; i--) {
-            if (arr[i][j] === "X") {
-              ans = false;
-              break;
-            }
-          }
-        } else {
-          for (let i = i1; i <= i2; i++) {
-            if (arr[i][j] === "X") {
-              ans = false;
-              break;
-            }
-          }
-        }
-        return ans;
+    let ans = 1;
+    function checkp(i, j) {
+      let c = 0;
+      if (i > 0 && arr[i - 1][j] === "P") {
+        c++;
       }
-      function gotoj(j1, j2, i) {
-        let ans = true;
-        if (j1 > j2) {
-          for (let j = j1; j >= j2; j--) {
-            if (arr[i][j] === "X") {
-              ans = false;
-              break;
-            }
-          }
-        } else {
-          for (let j = j1; j <= j2; j++) {
-            if (arr[i][j] === "X") {
-              ans = false;
-              break;
-            }
-          }
-        }
-        return ans;
+      if (i < 4 && arr[i + 1][j] === "P") {
+        c++;
       }
-
-      ans = gotoi(i1, i2, j1);
-
-      if (ans) {
-        ans = gotoj(j1, j2, i2);
+      if (j > 0 && arr[i][j - 1] === "P") {
+        c++;
       }
-      ans1 = gotoj(j1, j2, i1);
-
-      if (ans1) {
-        ans1 = gotoj(i1, i2, j2);
+      if (j < 4 && arr[i][j + 1] === "P") {
+        c++;
       }
-
-      return ans || ans1;
+      return c;
     }
+
     for (let i = 0; i < 5; i++) {
       for (let j = 0; j < 5; j++) {
-        if (arr[i][j] === "P") {
-          p[c] = [i, j];
-          c++;
-        }
-      }
-    }
-    for (let i = 0; i < c; i++) {
-      for (let j = i + 1; j < c; j++) {
-        let k = i !== j ? r(p[i], p[j]) : 0;
-        if (i !== j && k < 3) {
-          if (IsOpen(p[i], p[j])) {
-            ans = 0;
-            break;
-          }
+        if (arr[i][j] === "P" && checkp(i, j) > 0) {
+          ans = 0;
+          break;
+        } else if (arr[i][j] === "O" && checkp(i, j) > 1) {
+          ans = 0;
+          break;
         }
       }
       if (ans === 0) {
         break;
       }
     }
+
     return ans;
   }
 
   let ans = [];
+
   for (let i of places) {
     ans.push(check(i));
   }
 
   return ans;
 }
+let values = [
+  [
+    ["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"],
+    ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"],
+    ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"],
+    ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"],
+    ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"],
+  ],
+  [1, 0, 1, 1, 1],
+];
+
+console.log(
+  `calculated_result : ${solution(values[0])}, expected_result : ${values[1]} `
+);
+
+//2차 시도 [정확성: 94.0, 합계: 94.0 / 100.0]
+// function solution(places) {
+//   function r(a, b) {
+//     return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
+//   }
+
+//   function check(arr) {
+//     let p = [],
+//       c = 0,
+//       ans = 1;
+//     function IsOpen(a, b) {
+//       let [i1, j1] = a,
+//         [i2, j2] = b,
+//         ans = true,
+//         ans1 = true;
+//       let k1 = i2 - i1;
+//       function gotoi(i1, i2, j) {
+//         let ans = true;
+//         if (i1 > i2) {
+//           for (let i = i1; i >= i2; i--) {
+//             if (arr[i][j] === "X") {
+//               ans = false;
+//               break;
+//             }
+//           }
+//         } else {
+//           for (let i = i1; i <= i2; i++) {
+//             if (arr[i][j] === "X") {
+//               ans = false;
+//               break;
+//             }
+//           }
+//         }
+//         return ans;
+//       }
+//       function gotoj(j1, j2, i) {
+//         let ans = true;
+//         if (j1 > j2) {
+//           for (let j = j1; j >= j2; j--) {
+//             if (arr[i][j] === "X") {
+//               ans = false;
+//               break;
+//             }
+//           }
+//         } else {
+//           for (let j = j1; j <= j2; j++) {
+//             if (arr[i][j] === "X") {
+//               ans = false;
+//               break;
+//             }
+//           }
+//         }
+//         return ans;
+//       }
+
+//       ans = gotoi(i1, i2, j1);
+
+//       if (ans) {
+//         ans = gotoj(j1, j2, i2);
+//       }
+//       ans1 = gotoj(j1, j2, i1);
+
+//       if (ans1) {
+//         ans1 = gotoj(i1, i2, j2);
+//       }
+
+//       return ans || ans1;
+//     }
+//     for (let i = 0; i < 5; i++) {
+//       for (let j = 0; j < 5; j++) {
+//         if (arr[i][j] === "P") {
+//           p[c] = [i, j];
+//           c++;
+//         }
+//       }
+//     }
+//     for (let i = 0; i < c; i++) {
+//       for (let j = i + 1; j < c; j++) {
+//         let k = i !== j ? r(p[i], p[j]) : 0;
+//         if (i !== j && k < 3) {
+//           if (IsOpen(p[i], p[j])) {
+//             ans = 0;
+//             break;
+//           }
+//         }
+//       }
+//       if (ans === 0) {
+//         break;
+//       }
+//     }
+//     return ans;
+//   }
+
+//   let ans = [];
+//   for (let i of places) {
+//     ans.push(check(i));
+//   }
+
+//   return ans;
+// }
 
 // 첫시도 [정확성: 26.1,  합계: 26.1 / 100.0]
 // function solution(places) {
