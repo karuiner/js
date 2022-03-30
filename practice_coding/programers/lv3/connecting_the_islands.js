b0b2c3eff91e88e3dc3f0fb302b81300da693557
karuiner <intaekgong@gmail.com>
2022-03-30 21:15:19 +0900

    프로그래머스 섬 연결 하기 (Level 3) 풀이중

--- practice_coding/programers/lv3/connecting_the_islands.js
+++ practice_coding/programers/lv3/connecting_the_islands.js
@@ -1,124 +1,178 @@
 //섬 연결하기
 
-// 시도 3 - 미완성
+//시도 4  모든 경우의수 따지기 - 느리다
 function solution(n, costs) {
   let db = {};
   for (let [i, j, c] of costs) {
     if (db[i] === undefined) {
       db[i] = {};
     }
-
-    if (db[i][j] === undefined) {
-      db[i][j] = c;
-    }
-
     if (db[j] === undefined) {
       db[j] = {};
     }
-
-    if (db[j][i] === undefined) {
-      db[j][i] = c;
-    }
+    db[i][j] = c;
+    db[j][i] = c;
   }
-
-  let ans = Infinity;
-  for (let i = 0; i < n; i++) {
-    let c = 0,
-      ck = {};
-    function f(id, s, c) {
-      let ans = Infinity;
-      c++;
-      ck[id] = true;
-      if (c === n) {
-        return s;
-      }
-      let entries = Object.entries(db[id]);
-      let l = entries.length;
-      entries.sort((a, b) => a[1] - b[1]);
-      for (let i = 0; i < l; i++) {
-        let ns = s;
-        for (let j = i; j < l; j++) {
-          let [nid, nv] = entries[j];
-
-          let sub = f(nid, ns + nv);
-          ns = sub;
-          if (c === n) {
-            break;
+  function f(dp, s) {
+    let rt = [],
+      ans = Infinity;
+    for (let i = 0; i < n; i++) {
+      if (dp[i]) {
+        for (let j in db[i]) {
+          if (!dp[Number(j)]) {
+            rt.push([j, db[i][j]]);
           }
         }
-
-        if (ns < ans) {
-          ans = ns;
-        }
       }
-      return ans;
     }
-    let cc = f(i, 0);
+    if (rt.length === 0) {
+      return s;
+    }
+    for (let [j, v] of rt) {
+      let sub = [...dp];
 
-    if (cc < ans) {
-      ans = cc;
+      sub[Number(j)] = true;
+      let rt = f(sub, s + v);
+      if (rt < ans) {
+        ans = rt;
+      }
+    }
+    return ans;
+  }
+  let dp = Array(n).fill(false),
+    ans = Infinity;
+  for (let i = 0; i < n; i++) {
+    let sub = [...dp];
+    sub[Number(i)] = true;
+
+    let rst = f([...sub], 0);
+    if (rst < ans) {
+      ans = rst;
     }
   }
 
   return ans;
 }
+
+// 시도 3 - 미완성
+// function solution(n, costs) {
+//   let db = {};
+//   for (let [i, j, c] of costs) {
+//     if (db[i] === undefined) {
+//       db[i] = {};
+//     }
+
+//     if (db[i][j] === undefined) {
+//       db[i][j] = c;
+//     }
+
+//     if (db[j] === undefined) {
+//       db[j] = {};
+//     }
+
+//     if (db[j][i] === undefined) {
+//       db[j][i] = c;
+//     }
+//   }
+
+//   let ans = Infinity;
+//   for (let i = 0; i < n; i++) {
+//     let c = 0,
+//       ck = {};
+//     function f(id, s, c) {
+//       let ans = Infinity;
+//       c++;
+//       ck[id] = true;
+//       if (c === n) {
+//         return s;
+//       }
+//       let entries = Object.entries(db[id]);
+//       let l = entries.length;
+//       entries.sort((a, b) => a[1] - b[1]);
+//       for (let i = 0; i < l; i++) {
+//         let ns = s;
+//         for (let j = i; j < l; j++) {
+//           let [nid, nv] = entries[j];
+
+//           let sub = f(nid, ns + nv);
+//           ns = sub;
+//           if (c === n) {
+//             break;
+//           }
+//         }
+
+//         if (ns < ans) {
+//           ans = ns;
+//         }
+//       }
+//       return ans;
+//     }
+//     let cc = f(i, 0);
+
+//     if (cc < ans) {
+//       ans = cc;
+//     }
+//   }
+
+//   return ans;
+// }
 
 // 시도 2
-function solution(n, costs) {
-  let db = {};
-  for (let [i, j, c] of costs) {
-    if (db[i] === undefined) {
-      db[i] = {};
-    }
+// function solution(n, costs) {
+//   let db = {};
+//   for (let [i, j, c] of costs) {
+//     if (db[i] === undefined) {
+//       db[i] = {};
+//     }
 
-    if (db[i][j] === undefined) {
-      db[i][j] = c;
-    }
+//     if (db[i][j] === undefined) {
+//       db[i][j] = c;
+//     }
 
-    if (db[j] === undefined) {
-      db[j] = {};
-    }
+//     if (db[j] === undefined) {
+//       db[j] = {};
+//     }
 
-    if (db[j][i] === undefined) {
-      db[j][i] = c;
-    }
-  }
+//     if (db[j][i] === undefined) {
+//       db[j][i] = c;
+//     }
+//   }
 
-  let ans = Infinity;
-  for (let i = 0; i < n; i++) {
-    let c = 0,
-      ck = {};
-    function f(id, s) {
-      let ans = 0,
-        ns = s;
-      c++;
-      ck[id] = true;
-      if (c === n) {
-        return s;
-      }
-      let entries = Object.entries(db[id]);
-      entries.sort((a, b) => a[1] - b[1]);
-      for (let [nid, nv] of entries) {
-        if (ck[nid] === undefined) {
-          let sub = f(nid, ns + nv);
-          if (c === n) {
-            ans = sub;
-          } else {
-            ns = sub;
-          }
-        }
-      }
-      return c < n ? ns : ans;
-    }
-    let cc = f(i, 0);
+//   let ans = Infinity;
+//   for (let i = 0; i < n; i++) {
+//     let c = 0,
+//       ck = {};
+//     function f(id, s) {
+//       let ans = 0,
+//         ns = s;
+//       c++;
+//       ck[id] = true;
+//       if (c === n) {
+//         return s;
+//       }
+//       let entries = Object.entries(db[id]);
+//       entries.sort((a, b) => a[1] - b[1]);
+//       for (let [nid, nv] of entries) {
+//         if (ck[nid] === undefined) {
+//           let sub = f(nid, ns + nv);
+//           if (c === n) {
+//             ans = sub;
+//           } else {
+//             ns = sub;
+//           }
+//         }
+//       }
+//       return c < n ? ns : ans;
+//     }
+//     let cc = f(i, 0);
 
-    if (cc < ans) {
-      ans = cc;
-    }
-  }
+//     if (cc < ans) {
+//       ans = cc;
+//     }
+//   }
 
-  return ans;
-}
+//   return ans;
+// }
 
 // 시도 1
 // 한 노드에서 하나의 연결점을 가진다는 가정하에 구성된 코드이기에 한계를 가짐.
