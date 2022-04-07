93d571d3e133fa2884078e0d95ba00cf10c058b9
karuiner <intaekgong@gmail.com>
2022-04-08 02:37:30 +0900

    프로그래머스 표 편집 (Level 3) 풀이중

--- practice_coding/programers/lv3/Edit_table.js
+++ practice_coding/programers/lv3/Edit_table.js
@@ -1,55 +1,28 @@
 //표 편집
 
-// 시도 1보다는 좋은 정확성을 보이나 효율성은 더 나빠진 코드
+// 정확도 풀이 완료 효율성 전반 풀이 시도 1보다 속도는 느려짐
 function solution(n, k, cmd) {
   let ans = Array(n).fill("O"),
     r = [];
-
-  function leng(a) {
-    let b = k;
-    if (a > k) {
-      [a, b] = [k, a];
-    }
-    let l = b - a,
-      c = 0;
-    for (let i of r) {
-      if (i >= a && i < b) {
-        c--;
-      }
-    }
-    return l + c;
-  }
-
   function up(x) {
-    let [a, b] = [0, k];
-    while (a < b) {
-      let m = Math.floor((a + b) / 2);
-      let u = leng(m);
-      if (u > x) {
-        a = m + 1;
-      } else if (u < x) {
-        b = m;
-      } else {
-        a = m;
-        b = m;
+    let a = k;
+
+    while (a > 0 && x > 0) {
+      a--;
+      if (ans[a] === "O") {
+        x--;
       }
     }
-
     return a;
   }
 
   function down(x) {
-    let [a, b] = [k, n - 1];
-    while (a < b) {
-      let m = Math.floor((a + b) / 2);
-      let u = leng(m);
-      if (u > x) {
-        b = m;
-      } else if (u < x) {
-        a = m + 1;
-      } else {
-        a = m;
-        b = m;
+    let a = k;
+
+    while (a < n - 1 && x > 0) {
+      a++;
+      if (ans[a] === "O") {
+        x--;
       }
     }
     return a;
@@ -91,6 +64,98 @@ function solution(n, k, cmd) {
   }
   return ans.join("");
 }
+
+// 시도 1보다는 좋은 정확성을 보이나 효율성은 더 나빠진 코드
+// function solution(n, k, cmd) {
+//   let ans = Array(n).fill("O"),
+//     r = [];
+
+//   function leng(a) {
+//     let b = k;
+//     if (a > k) {
+//       [a, b] = [k, a];
+//     }
+//     let l = b - a,
+//       c = 0;
+//     for (let i of r) {
+//       if (i >= a && i < b) {
+//         c--;
+//       }
+//     }
+//     return l + c;
+//   }
+
+//   function up(x) {
+//     let [a, b] = [0, k];
+//     while (a < b) {
+//       let m = Math.floor((a + b) / 2);
+//       let u = leng(m);
+//       if (u > x) {
+//         a = m + 1;
+//       } else if (u < x) {
+//         b = m;
+//       } else {
+//         a = m;
+//         b = m;
+//       }
+//     }
+
+//     return a;
+//   }
+
+//   function down(x) {
+//     let [a, b] = [k, n - 1];
+//     while (a < b) {
+//       let m = Math.floor((a + b) / 2);
+//       let u = leng(m);
+//       if (u > x) {
+//         b = m;
+//       } else if (u < x) {
+//         a = m + 1;
+//       } else {
+//         a = m;
+//         b = m;
+//       }
+//     }
+//     return a;
+//   }
+
+//   for (let i of cmd) {
+//     if (i.length > 1) {
+//       let [cmd, x] = i.split(" ");
+//       if (cmd === "U") {
+//         k = up(Number(x));
+//       } else {
+//         k = down(Number(x));
+//       }
+//     } else {
+//       if (i === "C") {
+//         ans[k] = "X";
+//         r.push(k);
+//         let nk = -1;
+//         for (let j = k + 1; j < n; j++) {
+//           if (ans[j] == "O") {
+//             nk = j;
+//             break;
+//           }
+//         }
+//         if (nk === -1) {
+//           for (let j = k - 1; j >= 0; j--) {
+//             if (ans[j] == "O") {
+//               nk = j;
+//               break;
+//             }
+//           }
+//         }
+//         k = nk;
+//       } else {
+//         let j = r.pop();
+//         ans[j] = "O";
+//       }
+//     }
+//   }
+//   return ans.join("");
+// }
 
 //시도 2 더 좋지 못한 결과를 가짐
 // function solution(n, k, cmd) {
