f6c29cc94a187fa769e01ea3a243807f2e8c7c76
karuiner <intaekgong@gmail.com>
2022-04-13 20:39:15 +0900

    프로그래머스 거스름돈 (Level 3) 풀이완료

--- practice_coding/programers/lv3/change.js
+++ practice_coding/programers/lv3/change.js
@@ -1,32 +1,56 @@
 // 거스름 돈
-// 시도 중
+// 풀이 완료
 function solution(n, money) {
-  let db = {},
+  let dp = Array(n + 1).fill(0),
     l = money.length;
   money.sort((a, b) => a - b);
-  function f(c, k) {
-    let ans = 0;
-    if (c < money[0] || (k === 0 && c % money[k] !== 0)) {
-      db[`${c}-${k}`] = 0;
-    } else if (db[`${c}-${k}`] === undefined) {
-      for (let i = k; i >= 0; i--) {
-        if (c - money[i] > 0) {
-          ans += f(c - money[i], i);
-        } else if (c === money[i]) {
-          ans++;
-        }
-      }
-      db[`${c}-${k}`] = ans;
-    } else {
-      ans += db[`${c}-${k}`];
+  dp[0] = 1;
+  for (let i of money) {
+    let k = i;
+    for (let j = i; j <= n; j++) {
+      dp[j] += dp[j - i];
     }
-
-    return ans;
   }
 
-  let ans = f(n, l - 1);
-  return ans;
+  return dp[n];
 }
+let n = 5,
+  money = [1, 2, 5],
+  expected_result = 4;
+console.log(
+  `calculated_result : ${solution(
+    n,
+    money
+  )}, expected_result : ${expected_result} `
+);
+// 시도 중
+// function solution(n, money) {
+//   let db = {},
+//     l = money.length;
+//   money.sort((a, b) => a - b);
+//   function f(c, k) {
+//     let ans = 0;
+//     if (c < money[0] || (k === 0 && c % money[k] !== 0)) {
+//       db[`${c}-${k}`] = 0;
+//     } else if (db[`${c}-${k}`] === undefined) {
+//       for (let i = k; i >= 0; i--) {
+//         if (c - money[i] > 0) {
+//           ans += f(c - money[i], i);
+//         } else if (c === money[i]) {
+//           ans++;
+//         }
+//       }
+//       db[`${c}-${k}`] = ans;
+//     } else {
+//       ans += db[`${c}-${k}`];
+//     }
+
+//     return ans;
+//   }
+
+//   let ans = f(n, l - 1);
+//   return ans;
+// }
 //시도 1
 // function solution(n, money) {
 //     let ans = 0,i=0,l=money.length;
