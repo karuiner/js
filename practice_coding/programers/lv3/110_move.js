4aa30c3818b3106ffa816e84e4c99a2dabe2443e
karuiner <intaekgong@gmail.com>
2022-04-16 12:05:24 +0900

    프로그래머스 110옮기기 (Level 3) 풀이중

--- practice_coding/programers/lv3/110_move.js
+++ practice_coding/programers/lv3/110_move.js
@@ -1,5 +1,96 @@
 //110 옮기기
 
+// 시도 3
+function solution3(s) {
+  let ans = [];
+
+  function cut(s) {
+    return s.replace("110", "");
+  }
+  function cut2(s, c) {
+    let nc = [...s.matchAll(/110/g)].length;
+    return [s.replace(/110/g, ""), c + nc];
+  }
+  function ncut(s, k) {
+    let q = s.length;
+    for (let i = k; i < s.length - 2; i++) {
+      if (s.slice(i, i + 3) === "110") {
+        q = i;
+        break;
+      }
+    }
+    return q < s.length
+      ? [s.slice(0, q) + s.slice(q + 3), q - 3 < 0 ? 0 : q - 3]
+      : [s, q];
+  }
+
+  function f2(s, k, c) {
+    let [ns, q] = ncut(s, k);
+    while (q < ns.length) {
+      c++;
+      [ns, q] = ncut(ns, q);
+    }
+    return [ns, c];
+  }
+  function f(s, c) {
+    let ns = cut(s);
+    while (ns.length < s.length) {
+      s = ns;
+      c++;
+      ns = cut(s);
+    }
+    return c > 0 ? [ns, c] : [s, c];
+  }
+  function f3(s, c) {
+    let [ns, nc] = cut2(s, c);
+    while (ns.length < s.length) {
+      s = ns[(ns, nc)] = cut2(s, nc);
+    }
+    return c > 0 ? [ns, nc] : [s, c];
+  }
+
+  function recon(s, c) {
+    let k = s.lastIndexOf("0"),
+      p = s + "110";
+    let ns = "";
+    for (let i = 0; i < c; i++) {
+      ns += "110";
+    }
+    if (k >= 0) {
+      return s.slice(0, k + 1) + ns + s.slice(k + 1);
+    } else {
+      return ns + s;
+    }
+  }
+  function find(s) {
+    let k = s.length,
+      p = s + "110";
+
+    for (let i = 0; i < s.length; i++) {
+      let ns = s.slice(0, i) + "110" + s.slice(i);
+      if (ns < p) {
+        p = ns;
+        k = i;
+      }
+    }
+    return k;
+  }
+
+  for (let i of s) {
+    let [ns, c] = f(i, 0);
+    //      let [ns,c]=f2(i,0,0)
+    // let k=find(ns)
+    //   let sub=''
+    //   for (let j=0; j <c;j++){
+    //       sub+='110'
+    //   }
+    // ans.push(ns.slice(0,k)+sub+ns.slice(k))
+    ans.push(recon(ns, c));
+  }
+
+  return ans;
+}
+
 //시도 2
 function solution2(s) {
   let ans = [];
