2e13f830de8cf86fcf65cbd529671099893f1240
karuiner <intaekgong@gmail.com>
2022-04-30 23:04:35 +0900

    프로그래머스 순위 (Level 3) 풀이시도 중

--- practice_coding/programers/lv3/ranking.js
+++ practice_coding/programers/lv3/ranking.js
@@ -1,5 +1,94 @@
 // 순위
 
+// 풀이시도 3 풀이법에대한 고민중. 확정된 풀이법 구상이 안됨
+function solution(n, results) {
+  let ans = 0,
+    dp = [],
+    rank = [],
+    c = [],
+    t = [],
+    sub = [];
+  for (let i = 0; i < n; i++) {
+    dp[i] = { w: [], d: [] };
+    c[i] = 0;
+    rank[i] = -1;
+    sub.push(i);
+  }
+  for (let [w, d] of results) {
+    dp[d - 1].w.push(w - 1);
+    dp[w - 1].d.push(d - 1);
+    c[d - 1]++;
+    c[w - 1]++;
+    if (c[w - 1] === n - 1) {
+      let k = dp[w - 1].w.length;
+      rank[k] = w - 1;
+      t.push(w - 1);
+    }
+    if (c[d - 1] === n - 1) {
+      let k = dp[d - 1].w.length;
+      rank[k] = d - 1;
+      t.push(d - 1);
+    }
+  }
+  t.sort((a, b) => a - b);
+  function f(arr) {
+    let n = arr.length,
+      ans = false;
+    if (n === 2) {
+      let [a, b] = arr;
+      if (dp[a].w.includes(b) || dp[a].d.includes(b)) {
+        ans = true;
+      }
+    } else {
+      let a = arr[0],
+        sub = arr.slice(1);
+      let w = dp[a].w.filter((x) => sub.includes(x)),
+        d = dp[a].d.filter((x) => sub.includes(x));
+      console.log(a, sub, w, d);
+      if (w.length > 1) {
+        let q = f(w);
+        if (q) {
+          ans = q;
+        }
+      } else {
+        ans = true;
+      }
+      if (d.length > 1) {
+        let q = f(d);
+        if (q) {
+          ans = q;
+        }
+      } else {
+        ans = true;
+      }
+    }
+    return ans;
+  }
+  function f2(n, arr) {
+    if (n === 1 && arr.length === 1) {
+      return true;
+    }
+    let node = [];
+    for (let i = 0; i < arr.length; i++) {
+      node[i] = i;
+    }
+    for (let i of arr) {
+      console.log(
+        dp[i].w.filter((x) => arr.includes(x)),
+        dp[i].d.filter((x) => arr.includes(x))
+      );
+    }
+  }
+  console.log(dp, t);
+  if (t.length > 0) {
+    sub = [...dp[t[0]].w];
+  }
+  console.log(sub);
+  f2(sub.length, sub);
+
+  return ans;
+}
+
 // 풀이 시도 2
 // function solution(n, results) {
 //   let ans = 0,
