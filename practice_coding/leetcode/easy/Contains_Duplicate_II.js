e25a77b8be1f69fee5d81929dca2494e8fcc8a09
karuiner <intaekgong@gmail.com>
2022-05-09 23:50:45 +0900

    leetcode 219. Contains Duplicate II (easy) 풀이 완료

--- practice_coding/leetcode/easy/Contains_Duplicate_II.js
+++ practice_coding/leetcode/easy/Contains_Duplicate_II.js
@@ -4,6 +4,24 @@
  * @param {number} k
  * @return {boolean}
  */
+// 풀이 완료
+var containsNearbyDuplicate = function (nums, k) {
+  let n = nums.length,
+    ans = false,
+    db = {};
+  for (let i = 0; i < n; i++) {
+    if (db[nums[i]] === undefined || i - db[nums[i]] > k) {
+      db[nums[i]] = i;
+    } else {
+      ans = true;
+      break;
+    }
+  }
+  return ans;
+};
+
+// Runtime: 213 ms, faster than 35.31% of JavaScript online submissions for Contains Duplicate II.
+// Memory Usage: 63.8 MB, less than 53.25% of JavaScript online submissions for Contains Duplicate II.
 
 // 풀이 시도 1
 var containsNearbyDuplicate = function (nums, k) {
