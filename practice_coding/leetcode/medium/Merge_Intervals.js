95622470bfffc83bd4fb2980584546fe56a107d3
karuiner <intaekgong@gmail.com>
2022-05-12 21:33:06 +0900

    leetcode 56. Merge Intervals(medium) 풀이완료

--- practice_coding/leetcode/medium/Merge_Intervals.js
+++ practice_coding/leetcode/medium/Merge_Intervals.js
@@ -4,6 +4,66 @@
  * @return {number[][]}
  */
 
+// 풀이 완료
+var merge = function (intervals) {
+  let dp = Array(10000).fill(-1),
+    min = 10000,
+    max = 0,
+    ans = [],
+    k = 0;
+
+  for (let [a, b] of intervals) {
+    let z = -1,
+      x = -1;
+    min = a - 1 < min ? a - 1 : min;
+    max = b - 1 > max ? b - 1 : max;
+    if (dp[a - 1] === -1) {
+      z = k;
+      k++;
+    } else {
+      z = dp[a - 1];
+    }
+    if (dp[b - 1] !== -1) {
+      x = dp[b - 1];
+    }
+
+    for (let i = a - 1; i < b; i++) {
+      dp[i] = z;
+    }
+    if (x !== -1) {
+      let i = b;
+      while (dp[b] === x) {
+        dp[b] = z;
+        b++;
+      }
+    }
+  }
+  let q = -1,
+    mx = -1,
+    mn = -1;
+  for (let i = min; i <= max + 1; i++) {
+    if (q === -1 && dp[i] !== -1) {
+      mn = i + 1;
+      q = dp[i];
+    } else if (dp[i] !== q) {
+      if (dp[i] === -1) {
+        mx = i;
+        q = -1;
+        ans.push([mn, mx]);
+      } else {
+        ans.push([mn, i]);
+        mn = i + 1;
+        q = dp[i];
+      }
+    }
+  }
+
+  return ans;
+};
+
+// Runtime: 166 ms, faster than 34.45% of JavaScript online submissions for Merge Intervals.
+// Memory Usage: 52.6 MB, less than 5.87% of JavaScript online submissions for Merge Intervals.
+
 // 풀이시도  1  -  167 / 169 test cases passed. - Runtime Error - heap out of memory
 
 var merge = function (intervals) {
