212aaba965b5c48f274474570dc8255ff88184af
karuiner <intaekgong@gmail.com>
2022-05-20 21:40:40 +0900

    Leetcode 565. Array Nesting (medium) 풀이 완료

--- practice_coding/leetcode/medium/Array_Nesting.js
+++ practice_coding/leetcode/medium/Array_Nesting.js
@@ -3,21 +3,19 @@
  * @param {number[]} nums
  * @return {number}
  */
+
 var arrayNesting = function (nums) {
-  let db = {},
-    n = nums.length,
+  let n = nums.length,
     dp = Array(n).fill(true),
     ans = 0;
-  for (let i = 0; i < n; i++) {
-    db[i] = nums[i];
-  }
+
   for (let i = 0; i < n; i++) {
     if (dp[i]) {
       let c = i,
         k = 0;
       while (dp[c]) {
         dp[c] = false;
-        c = db[c];
+        c = nums[c];
         k++;
       }
       ans = ans < k ? k : ans;
@@ -25,5 +23,29 @@ var arrayNesting = function (nums) {
   }
   return ans;
 };
+// Runtime: 102 ms, faster than 98.41% of JavaScript online submissions for Array Nesting.
+// Memory Usage: 54.2 MB, less than 85.71% of JavaScript online submissions for Array Nesting.
+// var arrayNesting = function (nums) {
+//   let db = {},
+//     n = nums.length,
+//     dp = Array(n).fill(true),
+//     ans = 0;
+//   for (let i = 0; i < n; i++) {
+//     db[i] = nums[i];
+//   }
+//   for (let i = 0; i < n; i++) {
+//     if (dp[i]) {
+//       let c = i,
+//         k = 0;
+//       while (dp[c]) {
+//         dp[c] = false;
+//         c = db[c];
+//         k++;
+//       }
+//       ans = ans < k ? k : ans;
+//     }
+//   }
+//   return ans;
+// };
 // Runtime: 139 ms, faster than 90.48% of JavaScript online submissions for Array Nesting.
 // Memory Usage: 59.8 MB, less than 82.54% of JavaScript online submissions for Array Nesting.
