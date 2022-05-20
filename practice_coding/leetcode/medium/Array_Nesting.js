f109e76918f7fa69ffebe0a44eb90101b55ddb74
karuiner <intaekgong@gmail.com>
2022-05-20 21:35:46 +0900

    Leetcode 565. Array Nesting (medium) 풀이 완료

---
+++ practice_coding/leetcode/medium/Array_Nesting.js
@@ -0,0 +1,29 @@
+/*
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arrayNesting = function (nums) {
+  let db = {},
+    n = nums.length,
+    dp = Array(n).fill(true),
+    ans = 0;
+  for (let i = 0; i < n; i++) {
+    db[i] = nums[i];
+  }
+  for (let i = 0; i < n; i++) {
+    if (dp[i]) {
+      let c = i,
+        k = 0;
+      while (dp[c]) {
+        dp[c] = false;
+        c = db[c];
+        k++;
+      }
+      ans = ans < k ? k : ans;
+    }
+  }
+  return ans;
+};
+// Runtime: 139 ms, faster than 90.48% of JavaScript online submissions for Array Nesting.
+// Memory Usage: 59.8 MB, less than 82.54% of JavaScript online submissions for Array Nesting.
