a2cc0158a60541591b16b1facc089608e4cea1fe
karuiner <intaekgong@gmail.com>
2022-06-01 23:16:47 +0900

    leetcode 2195. Append K Integers With Minimal Sum (medium) 풀이완료

--- practice_coding/leetcode/medium/Append_K_Integers_With_Minimal_Sum.js
+++ practice_coding/leetcode/medium/Append_K_Integers_With_Minimal_Sum.js
@@ -4,13 +4,14 @@
  * @param {number} k
  * @return {number}
  */
+
 var minimalKSum = function (nums, k) {
   let s = 0,
-    db = {};
+    y = 0;
   nums.sort((a, b) => a - b);
   nums.forEach((x) => {
-    if (x <= k && db[x] === undefined) {
-      db[x] = true;
+    if (x <= k && x > y) {
+      y = x;
       k++;
       s += x;
     }
@@ -18,6 +19,24 @@ var minimalKSum = function (nums, k) {
 
   return (k * (k + 1)) / 2 - s;
 };
+
+// Runtime: 194 ms, faster than 74.47% of JavaScript online submissions for Append K Integers With Minimal Sum.
+// Memory Usage: 54.6 MB, less than 97.87% of JavaScript online submissions for Append K Integers With Minimal Sum.
+
+// var minimalKSum = function (nums, k) {
+//   let s = 0,
+//     db = {};
+//   nums.sort((a, b) => a - b);
+//   nums.forEach((x) => {
+//     if (x <= k && db[x] === undefined) {
+//       db[x] = true;
+//       k++;
+//       s += x;
+//     }
+//   });
+
+//   return (k * (k + 1)) / 2 - s;
+// };
 
 // Runtime: 284 ms, faster than 21.28% of JavaScript online submissions for Append K Integers With Minimal Sum.
 // Memory Usage: 66 MB, less than 51.06% of JavaScript online submissions for Append K Integers With Minimal Sum.
