95c6a413d95e364c9b2ca725b2441502ac587b12
karuiner <intaekgong@gmail.com>
2022-06-18 23:04:41 +0900

    프로그래머스 파괴되지 않은 건물(Level 3) 풀이중

--- practice_coding/programers/lv3/undestroyed_building.js
+++ practice_coding/programers/lv3/undestroyed_building.js
@@ -1,5 +1,6 @@
 //파괴되지 않은 빌딩
-// 시도 1
+
+// 시도 2
 function solution(board, skill) {
   let m = board.length,
     n = board[0].length,
@@ -20,3 +21,25 @@ function solution(board, skill) {
 
   return ans;
 }
+
+// 시도 1
+// function solution(board, skill) {
+//   let m = board.length,
+//     n = board[0].length,
+//     ans = 0;
+
+//   for (let i = 0; i < m; i++) {
+//     for (let j = 0; j < n; j++) {
+//       for (let [t, r1, c1, r2, c2, d] of skill) {
+//         if (i >= r1 && i <= r2 && j >= c1 && j <= c2) {
+//           board[i][j] += t === 1 ? -d : d;
+//         }
+//       }
+//       if (board[i][j] > 0) {
+//         ans++;
+//       }
+//     }
+//   }
+
+//   return ans;
+// }
