bbc9bead20ea1b920f07adc663b0b139acaba8d3
karuiner <intaekgong@gmail.com>
2022-06-19 22:49:47 +0900

    프로그래머스 파괴되지 않은 건물(Level 3) 풀이완료

--- practice_coding/programers/lv3/undestroyed_building.js
+++ practice_coding/programers/lv3/undestroyed_building.js
@@ -1,26 +1,73 @@
 //파괴되지 않은 빌딩
 
-// 시도 2
 function solution(board, skill) {
   let m = board.length,
     n = board[0].length,
-    ans = 0;
-
+    ans = 0,
+    db = {},
+    dummy = [];
   for (let i = 0; i < m; i++) {
+    dummy[i] = Array(n).fill(0);
+  }
+
+  for (let [t, r1, c1, r2, c2, d] of skill) {
+    let k = t === 1 ? -d : d;
+    dummy[r1][c1] += k;
+    if (r2 + 1 < m) {
+      dummy[r2 + 1][c1] += -k;
+    }
+    if (c2 + 1 < n) {
+      dummy[r1][c2 + 1] += -k;
+    }
+    if (r2 + 1 < m && c2 + 1 < n) {
+      dummy[r2 + 1][c2 + 1] += k;
+    }
+  }
+
+  function f(mdata) {
     for (let j = 0; j < n; j++) {
-      for (let [t, r1, c1, r2, c2, d] of skill) {
-        if (i >= r1 && i <= r2 && j >= c1 && j <= c2) {
-          board[i][j] += t === 1 ? -d : d;
-        }
+      for (let i = 1; i < m; i++) {
+        dummy[i][j] += dummy[i - 1][j];
       }
-      if (board[i][j] > 0) {
-        ans++;
+    }
+    for (let i = 0; i < m; i++) {
+      for (let j = 0; j < n; j++) {
+        if (j > 0) {
+          dummy[i][j] += dummy[i][j - 1];
+        }
+        if (board[i][j] + dummy[i][j] > 0) {
+          ans++;
+        }
       }
     }
   }
 
+  f(dummy);
+
   return ans;
 }
+
+// 시도 2
+// function solution(board, skill) {
+//   let m = board.length,
+//     n = board[0].length,
+//     ans = 0;
+
+//   for (let i = 0; i < m; i++) {
+//     for (let j = 0; j < n; j++) {
+//       for (let [t, r1, c1, r2, c2, d] of skill) {
+//         if (i >= r1 && i <= r2 && j >= c1 && j <= c2) {
+//           board[i][j] += t === 1 ? -d : d;
+//         }
+//       }
+//       if (board[i][j] > 0) {
+//         ans++;
+//       }
+//     }
+//   }
+
+//   return ans;
+// }
 
 // 시도 1
 // function solution(board, skill) {
