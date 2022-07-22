6223b593b5af2f2571ed324b47c774f7dc3a9c32
karuiner <intaekgong@gmail.com>
2022-07-22 21:13:02 +0900

    leetcode  833. Find And Replace in String(medium) 풀이완료

--- practice_coding/leetcode/medium/Find_And_Replace_in_String.js
+++ practice_coding/leetcode/medium/Find_And_Replace_in_String.js
@@ -6,10 +6,11 @@
  * @param {string[]} targets
  * @return {string}
  */
-//풀이 시도 1
+// 풀이 완료
 var findReplaceString = function (s, indices, sources, targets) {
   let ans = "",
-    k = 0;
+    k = 0,
+    check = true;
   let dummy = indices.map((x, i) => [x, i]);
   dummy.sort((a, b) => {
     return a[0] - b[0];
@@ -19,6 +20,11 @@ var findReplaceString = function (s, indices, sources, targets) {
     let i = dummy[ii][1];
     let l = sources[i].length,
       j = indices[i];
+
+    if (j < k) {
+      check = false;
+      break;
+    }
     ans += s.slice(k, j);
 
     if (s.slice(j, j + l) === sources[i]) {
@@ -28,6 +34,39 @@ var findReplaceString = function (s, indices, sources, targets) {
     }
     k = j + l;
   }
-  ans += s.slice(k, s.length);
+  if (check) {
+    ans += s.slice(k, s.length);
+  } else {
+    ans = s;
+  }
   return ans;
 };
+
+// Runtime: 64 ms, faster than 94.49% of JavaScript online submissions for Find And Replace in String.
+// Memory Usage: 42.7 MB, less than 77.12% of JavaScript online submissions for Find And Replace in String.
+
+//풀이 시도 1
+// var findReplaceString = function (s, indices, sources, targets) {
+//   let ans = "",
+//     k = 0;
+//   let dummy = indices.map((x, i) => [x, i]);
+//   dummy.sort((a, b) => {
+//     return a[0] - b[0];
+//   });
+
+//   for (let ii = 0; ii < indices.length; ii++) {
+//     let i = dummy[ii][1];
+//     let l = sources[i].length,
+//       j = indices[i];
+//     ans += s.slice(k, j);
+
+//     if (s.slice(j, j + l) === sources[i]) {
+//       ans += targets[i];
+//     } else {
+//       ans += s.slice(j, j + l);
+//     }
+//     k = j + l;
+//   }
+//   ans += s.slice(k, s.length);
+//   return ans;
+// };
