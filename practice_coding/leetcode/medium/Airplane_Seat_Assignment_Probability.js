40b6e6d4ac0516c5f68d556cfcf7e6929f7cbdbc
karuiner <intaekgong@gmail.com>
2022-07-27 21:44:54 +0900

    leetcode 1227. Airplane Seat Assignment Probability(medium) 풀이 완료

---
+++ practice_coding/leetcode/medium/Airplane_Seat_Assignment_Probability.js
@@ -0,0 +1,12 @@
+/*
+*
+ * @param {number} n
+ * @return {number}
+ */
+ var nthPersonGetsNthSeat = function(n) {
+    return n >1?0.5:1
+};
+
+
+Runtime: 82 ms, faster than 65.79% of JavaScript online submissions for Airplane Seat Assignment Probability.
+Memory Usage: 42 MB, less than 26.32% of JavaScript online submissions for Airplane Seat Assignment Probability.
