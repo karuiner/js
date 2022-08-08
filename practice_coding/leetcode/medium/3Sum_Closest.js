0d97ba1d9839364fc0ec2b8eb15b644a583a363b
karuiner <intaekgong@gmail.com>
2022-08-08 22:42:13 +0900

    leetcode 16. 3Sum Closest (medium) 풀이중

--- practice_coding/leetcode/medium/3Sum_Closest.js
+++ practice_coding/leetcode/medium/3Sum_Closest.js
@@ -4,6 +4,33 @@
  * @param {number} target
  * @return {number}
  */
+// 풀이시도 2
+var threeSumClosest = function (nums, target) {
+  let ans = 1000000,
+    n = nums.length;
+  let i = 0,
+    j = n - 1;
+  while (j - i >= 2) {
+    let a = nums[i] + nums[i + 1] + nums[j],
+      b = nums[i] + nums[j - 1] + nums[j];
+    let k1 = Math.abs(a - target),
+      k2 = Math.abs(b - target);
+    console.log(a, b, k1, k2);
+    if (k1 > k2) {
+      if (Math.abs(ans - target) > k2) {
+        ans = b;
+      }
+      i++;
+    } else {
+      if (Math.abs(ans - target) > k1) {
+        ans = a;
+      }
+      j--;
+    }
+  }
+  return ans;
+};
+// 풀이시도 1
 var threeSumClosest = function (nums, target) {
   let ans = 1000000,
     n = nums.length;
