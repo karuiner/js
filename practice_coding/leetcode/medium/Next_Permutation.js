1c1717c6aa11777dd92fa2c8c4aadb64541c43c3
karuiner <intaekgong@gmail.com>
2022-08-16 23:22:06 +0900

    leetcode 31. Next Permutation(medium) 풀이완료

--- practice_coding/leetcode/medium/Next_Permutation.js
+++ practice_coding/leetcode/medium/Next_Permutation.js
@@ -3,6 +3,63 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
+
+// 풀이 완료
+var nextPermutation = function (nums) {
+  let b = [...nums],
+    n = nums.length,
+    db = {};
+  if (n === 1) {
+    return nums;
+  }
+
+  function f(arr, l) {
+    let an = arr.length;
+    let a = arr[0],
+      b = arr.slice(1);
+    if (an > 2) {
+      let s = f(b, l + 1);
+
+      if (s && a < b[0]) {
+        arr.sort((a, b) => a - b);
+        let q = [],
+          check = true,
+          k = -1;
+        arr.forEach((x) => {
+          if (x > a && check) {
+            k = x;
+            check = false;
+          } else {
+            q.push(x);
+          }
+        });
+
+        nums[l] = k;
+        for (let i = l + 1; i < n; i++) {
+          nums[i] = q[i - (l + 1)];
+        }
+        return false;
+      }
+      return true && s;
+    } else {
+      if (a >= b[0]) {
+        return true;
+      } else {
+        [nums[l], nums[l + 1]] = [nums[l + 1], nums[l]];
+        return false;
+      }
+    }
+  }
+  let ans = f(nums, 0);
+  if (ans) {
+    nums.sort((a, b) => a - b);
+  }
+};
+
+// Runtime: 134 ms, faster than 12.80% of JavaScript online submissions for Next Permutation.
+// Memory Usage: 44 MB, less than 24.83% of JavaScript online submissions for Next Permutation.
+
+// 풀이시도 1
 var nextPermutation = function (nums) {
   let b = [...nums],
     n = nums.length;
