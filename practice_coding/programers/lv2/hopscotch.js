2263d18326434c61eb891a34e2b498a785809457
karuiner <intaekgong@gmail.com>
2022-09-27 22:52:37 +0900

    프로그래머스 땅따먹기 (Level 2) 풀이중

--- practice_coding/programers/lv2/hopscotch.js
+++ practice_coding/programers/lv2/hopscotch.js
@@ -1,5 +1,54 @@
 //땅따먹기
 
+// 풀이 시도 2 - 고민중
+function solution(land) {
+  let ans = 0,
+    n = land.length,
+    m = land[0].length,
+    sub = [];
+  let dummy = [];
+  for (let i = 0; i < 4; i++) {
+    dummy[i] = [];
+    for (let j = 0; j < 4; j++) {
+      dummy[i][j] = 0;
+    }
+  }
+  for (let i = 0; i < n; i++) {
+    let dummy = [];
+    for (let i = 0; i < 4; i++) {
+      dummy[i] = [];
+      for (let j = 0; j < 4; j++) {
+        dummy[i][j] = 0;
+      }
+    }
+    sub[i] = dummy;
+  }
+
+  function f(from, idx) {
+    console.log(from, idx);
+    if (idx === n) {
+      console.log(sub[n - 1]);
+      return 0;
+    }
+
+    for (let i = 0; i < 4; i++) {
+      if (from !== i) {
+        if (idx > 0) {
+          let max = Math.max(...sub[idx][from]);
+          sub[idx][i][from] = max + land[idx][i];
+        } else {
+          for (let j = 0; j < 4; j++) {
+            sub[idx][i][j] = land[idx][i];
+          }
+        }
+      }
+      f(i, idx + 1);
+    }
+  }
+  f(-1, 0);
+  return 0;
+}
+
 //풀이 시도 1
 // 현재시점에서 가장 크게되는 결과로(탐욕법?) - 문제에서 주어진 보기 예문을 제외한 모든 예문 풀이실패
 
