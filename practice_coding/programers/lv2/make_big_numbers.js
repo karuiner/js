005d546b22ffd4c7b6c0358a1df9ff5bb1fd52ed
karuiner <intaekgong@gmail.com>
2022-10-01 21:52:00 +0900

    프로그래머스 큰 수 만들기 (Level 2) 풀이중

--- practice_coding/programers/lv2/make_big_numbers.js
+++ practice_coding/programers/lv2/make_big_numbers.js
@@ -1,5 +1,44 @@
 // 큰 수 만들기 - 다시 시작
 
+//풀이시도 2
+function solution(number, k) {
+  let idx = 0,
+    c = 0,
+    n = number.length,
+    p = 0;
+  let arr = [],
+    check = [];
+
+  function f(s, c) {
+    let ans = "",
+      n = s.length,
+      x = "";
+
+    for (let i = 0; i < n; i++) {
+      let sub = s.slice(0, i) + s.slice(i + 1);
+
+      if (x.length === 0) {
+        x = sub[0];
+      } else if (Number(x) < Number(sub[0])) {
+        x = sub[0];
+      }
+      if (x === sub[0]) {
+        if (c > 1) {
+          sub = f(sub, c - 1);
+        }
+        if (ans.length === 0) {
+          ans = sub;
+        } else if (sub > ans) {
+          ans = sub;
+        }
+      }
+    }
+    return ans;
+  }
+
+  return f(number, k);
+}
+
 // 풀이시도 1
 function solution(number, k) {
   let i = 0,
