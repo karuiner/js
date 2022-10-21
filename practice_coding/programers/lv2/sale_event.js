05428c69e359cdf64735d4e6dbaaca4aa51a0896
karuiner <intaekgong@gmail.com>
2022-10-21 23:24:23 +0900

    프로그래머스 할인 행사(Level 2) 풀이 완료

--- practice_coding/programers/lv2/sale_event.js
+++ practice_coding/programers/lv2/sale_event.js
@@ -1,5 +1,42 @@
 //할인 행사
 
+// 풀이 완료
+function solution(want, number, discount) {
+  let db = {},
+    n = discount.length,
+    n1 = want.length,
+    ans = 0;
+
+  for (let i = 0; i < 10; i++) {
+    if (db[discount[i]] === undefined) {
+      db[discount[i]] = 0;
+    }
+    db[discount[i]]++;
+  }
+
+  for (let i = 0; i < n - 9; i++) {
+    let check = true;
+    for (let j = 0; j < n1; j++) {
+      if (db[want[j]] === undefined || db[want[j]] < number[j]) {
+        check = false;
+        break;
+      }
+    }
+    if (check) {
+      ans++;
+    }
+    if (db[discount[i]] > 0) {
+      db[discount[i]]--;
+    }
+    if (db[discount[i + 10]] === undefined) {
+      db[discount[i + 10]] = 0;
+    }
+    db[discount[i + 10]]++;
+  }
+
+  return ans;
+}
+
 // 풀이 시도 1
 function solution(want, number, discount) {
   let db = {},
