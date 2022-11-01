d556bd107d9094451328100d6598c4760e804cd4
karuiner <intaekgong@gmail.com>
2022-11-01 21:23:45 +0900

    프로그래머스 야간 전술 보행(Level 1) 풀이 완료

--- practice_coding/programers/lv2/night_tactical_walking.js
+++ practice_coding/programers/lv2/night_tactical_walking.js
@@ -1,4 +1,37 @@
 // 야간 전술 보행
+// 풀이 완료
+function solution(distance, scope, times) {
+  let ans = distance,
+    n = scope.length;
+  function f(t, time) {
+    let [w, r] = time,
+      f = w + r;
+    if (t % f === 0) {
+      return true;
+    } else {
+      let k = Math.floor(t / f);
+
+      let s = k * f;
+      return t > s + w;
+    }
+  }
+  for (let i = 0; i < n; i++) {
+    let [s, e] = scope[i],
+      [w, r] = times[i];
+    if (s > e) {
+      [s, e] = [e, s];
+    }
+
+    for (let j = s; j < e + 1; j++) {
+      if (!f(j, [w, r])) {
+        ans = ans > j ? j : ans;
+        break;
+      }
+    }
+  }
+
+  return ans;
+}
 
 // 풀이 시도 1
 function solution(distance, scope, times) {
