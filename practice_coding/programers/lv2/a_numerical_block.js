b8c8206b6ef429d881da07d62a93c1d1b38181d0
karuiner <intaekgong@gmail.com>
2022-11-08 23:25:25 +0900

    프로그래머스 숫자 블록 (Level 2) 풀이중

--- practice_coding/programers/lv2/a_numerical_block.js
+++ practice_coding/programers/lv2/a_numerical_block.js
@@ -1,5 +1,32 @@
 // 숫바 블록
 
+// 풀이 시도2
+function solution(begin, end) {
+  let ans = [];
+  function f(x) {
+    if (x === 1) return 0;
+    let ans = 1,
+      s = 2;
+    let z = Math.floor(Math.sqrt(x));
+    if (x >= 20000000) {
+      s = Math.floor(x / 10000000);
+    }
+
+    for (let i = s; i <= z; i++) {
+      if (x % i === 0) {
+        ans = x / i;
+        break;
+      }
+    }
+    return ans;
+  }
+  for (let i = begin; i <= end; i++) {
+    ans.push(f(i));
+  }
+
+  return ans;
+}
+
 // 풀이 시도 1
 function solution(begin, end) {
   let ans = [];
