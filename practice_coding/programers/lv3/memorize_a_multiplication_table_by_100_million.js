a30dec0770f6e5a4b2d5a08de784a6bd6efb6d9c
karuiner <intaekgong@gmail.com>
2022-11-26 22:52:51 +0900

    프로그래머스 억억단을 외우자 (Level 3) 풀이중

--- practice_coding/programers/lv3/memorize_a_multiplication_table_by_100_million.js
+++ practice_coding/programers/lv3/memorize_a_multiplication_table_by_100_million.js
@@ -1,5 +1,43 @@
 //억억단을 외우자
 
+// 풀이 시도 2
+function solution(e, starts) {
+  let ans = [],
+    max = 0,
+    marr = [],
+    db = {};
+
+  function f(x) {
+    let h = Math.floor(Math.sqrt(x)),
+      ans = 0;
+    for (let i = 1; i <= h; i++) {
+      if (x % i === 0) {
+        if (i === x / i) {
+          ans++;
+        } else {
+          ans += 2;
+        }
+      }
+    }
+    return ans;
+  }
+  for (let i = 0; i < starts.length; i++) {
+    let k = starts[i];
+    let max = 0;
+    for (let j = k; j <= e; j++) {
+      if (db[j] === undefined) {
+        db[j] = f(j);
+      }
+      if (db[j] > max) {
+        max = db[j];
+        ans[i] = j;
+      }
+    }
+  }
+
+  return ans;
+}
+
 // 수정중
 function solution(e, starts) {
   let ans = [],
