4e3fc3fd1622c372113d0814ce872015f8b6d9d7
karuiner <intaekgong@gmail.com>
2022-12-07 23:21:14 +0900

    프로그래머스 양과 늑대 (Level 3) 풀이중

--- practice_coding/programers/lv3/sheep_and_wolf.js
+++ practice_coding/programers/lv3/sheep_and_wolf.js
@@ -1,5 +1,71 @@
 //양과 늑대
 
+// 풀이 시도 1
+function solution(info, edges) {
+  let ans = 0,
+    wolf = 0,
+    arr = [0],
+    db = {},
+    sheep = [];
+  for (let i = 0; i < info.length; i++) {
+    if (info[i] === 0) {
+      sheep.push([i, 0]);
+    }
+  }
+
+  for (let [i, j] of edges) {
+    if (db[i] === undefined) {
+      db[i] = [];
+    }
+    arr[j] = i;
+    db[i].push(j);
+  }
+  for (let i = 0; i < sheep.length; i++) {
+    let k = sheep[i][0],
+      s = 0;
+    while (k !== 0) {
+      if (info[k]) {
+        s++;
+      }
+      k = arr[k];
+    }
+    sheep[i][1] = s;
+  }
+  sheep.sort((a, b) => {
+    return a[1] - b[1] !== 0 ? a[1] - b[1] : a[0] - b[0];
+  });
+  for (let [i, j] of sheep) {
+    let k = i,
+      p = [ans, wolf];
+    if (k !== 0) {
+      while (k !== 0) {
+        if (info[k] == 0) {
+          p[0]++;
+        } else if (info[k] === 1) {
+          p[1]++;
+        }
+        if (info[k] >= 0) {
+          info[k] = -1;
+        }
+        k = arr[k];
+      }
+    } else {
+      if (info[k] === 0) {
+        p[0]++;
+      } else if (info[k] === 1) {
+        p[1]++;
+      }
+      info[k] = -1;
+    }
+    if (ans > 0 ? ans > p[1] : ans >= p[1]) {
+      [ans, wolf] = p;
+    } else {
+      break;
+    }
+  }
+
+  return ans;
+}
 // 풀이 시도
 function solution(info, edges) {
   let ans = 0,
