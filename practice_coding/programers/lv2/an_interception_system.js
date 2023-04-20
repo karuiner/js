993b36d0f9eba1fd225aca7fe7bd61f8ecf8dff2
karuiner <intaekgong@gmail.com>
2023-04-20 22:44:01 +0900

    프로그래머스 요격 시스템 (Level 2) 3차 시도

--- practice_coding/programers/lv2/an_interception_system.js
+++ practice_coding/programers/lv2/an_interception_system.js
@@ -1,4 +1,32 @@
 //요격 시스템
+// 3차 시도 실패
+// 더 효율적인 코드를 작성해야 한다.
+function solution(targets) {
+  let ans = 0,
+    n = targets.length;
+  targets.sort((a, b) => a[1] - b[1]);
+
+  function get(arr) {
+    let [a, b] = arr[0],
+      ans = [];
+    for (let i = 1; i < arr.length; i++) {
+      let [x, y] = arr[i];
+      if (y <= a || x >= b) {
+        ans.push(arr[i]);
+      } else {
+        [a, b] = [Math.max(a, x), Math.min(b, y)];
+      }
+    }
+    return ans;
+  }
+  while (targets.length > 0) {
+    targets = get(targets);
+    ans++;
+  }
+
+  return ans;
+}
+
 // 2차 시도 실패
 // 정렬을 끝부분의 위치에따라 오름차순으로 정렬 함. 한쪽끝에서부터 일치하는 뭉치를 잘라내는 방식
 // 하지만 현재 코드는 모든 케이스를 모두 학인하고 있고 필요없는것도 다시 확인 하고 있기때문에
