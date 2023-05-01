3073be38e5170f49c4e13f97c5c42bac470533ad
karuiner <intaekgong@gmail.com>
2023-05-01 22:15:02 +0900

    프로그래머스  뒤에 있는 큰 수 찾기 (Level 2) 풀이 시도 2

--- practice_coding/programers/lv2/Find_the_large_number_behind_you.js
+++ practice_coding/programers/lv2/Find_the_large_number_behind_you.js
@@ -1,5 +1,36 @@
 //뒤에 있는 큰 수 찾기
 
+//  풀이 시도 2 실패
+
+function solution(numbers) {
+  let ans = [],
+    n = numbers.length,
+    position = Array(n).fill(-1);
+  for (let i = n - 1; i >= 0; i--) {
+    let a = numbers[i],
+      b = numbers[i + 1];
+    if (b === undefined) {
+      ans[i] = -1;
+    } else if (a < b) {
+      ans[i] = b;
+      position[i] = i + 1;
+    } else {
+      let k = position[i + 1];
+      while (k > 0 && a > numbers[k]) {
+        k = position[k];
+      }
+      if (k < 0) {
+        position[i] = -1;
+        ans[i] = -1;
+      } else {
+        position[i] = k;
+        ans[i] = numbers[k];
+      }
+    }
+  }
+  return ans;
+}
+
 // 풀이 시도 1 실패
 // 긴 예제의경우 시간 초과 가 생김
 // 계산 단축 방법을 고민할것
