584b99235acbfa54377f5655f6fb6434948f20dc
karuiner <intaekgong@gmail.com>
2023-05-02 22:22:38 +0900

    프로그래머스  뒤에 있는 큰 수 찾기 (Level 2) 풀이 시도 3

--- practice_coding/programers/lv2/Find_the_large_number_behind_you.js
+++ practice_coding/programers/lv2/Find_the_large_number_behind_you.js
@@ -1,5 +1,50 @@
 //뒤에 있는 큰 수 찾기
 
+//풀이시도 3 실패
+// 더나쁜 결과
+function solution(numbers) {
+  // 뒤에서 부터 확인하는 경우
+  // 1. 해당 값이 마지막일경우 - -1 을 부여
+  // 2. 뒷값이 현재 값보다 큰경우 뒷값을 부여
+  //
+
+  let ans = [-1],
+    n = numbers.length,
+    position = Array(n).fill(-1),
+    k = -1;
+
+  function check(a, b) {
+    if (b === undefined) {
+      return ans[0];
+    } else if (a >= b) {
+      let s = ans[ans.length - 1],
+        check = false;
+      for (let i of ans) {
+        if (i > a) {
+          s = i;
+          ans = [s, ...ans];
+          check = true;
+          break;
+        }
+      }
+      if (!check) {
+        ans = [s, ...ans];
+      }
+      return s;
+    } else {
+      ans = [b, ...ans];
+      return b;
+    }
+  }
+
+  for (let i = n - 1; i >= 0; i--) {
+    let a = numbers[i],
+      b = numbers[i + 1];
+    check(a, b, ans);
+  }
+  return ans;
+}
+
 //  풀이 시도 2 실패
 
 function solution(numbers) {
