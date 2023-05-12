a30b60312e737a0d6a0097c433c5c25ff94953e2
karuiner <intaekgong@gmail.com>
2023-05-12 22:21:14 +0900

    프로그래머스 마법의 엘레베이터(Level 2) 풀이완료

--- practice_coding/programers/lv2/magic_elevator.js
+++ practice_coding/programers/lv2/magic_elevator.js
@@ -1,4 +1,35 @@
 // 마법의 엘레베이터
+// 풀이 완료
+// 풀이 방식은 1의 자리수 부터 10이나 0을 맞춰 가능 식으로
+// 최고 자리수 까지 진행하는 것이다.
+// 다만 수에 따라 5 인 부분을 처리하는 방식이 달라질 것이다.
+// 따라서 두경우 모두 계산하고 최소가 되는 것을 결과로 돌려주게 구성하였다.
+function solution(storey) {
+  function f(n, t = false) {
+    let c = 0;
+    while (n > 0) {
+      let s = `${n}`,
+        l = s.length,
+        i = Number(s[l - 1]);
+      if (i > 5) {
+        c += 10 - i;
+        n = Math.floor((n + 10 - i) / 10);
+      } else if (i < 5) {
+        c += i;
+        n = Math.floor((n - i) / 10);
+      } else if (t) {
+        c += 10 - i;
+        n = Math.floor((n + 10 - i) / 10);
+      } else {
+        c += i;
+        n = Math.floor((n - i) / 10);
+      }
+    }
+    return c;
+  }
+
+  return Math.min(f(storey), f(storey, true));
+}
 
 // 풀이 시도 3
 function solution(storey) {
