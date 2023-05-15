513bd27cad9cd41a290111d05ba724ec0c54883c
karuiner <intaekgong@gmail.com>
2023-05-15 20:56:46 +0900

    프로그래머스 디펜스 게임 (Level 2) 풀이중

--- practice_coding/programers/lv2/defence_game.js
+++ practice_coding/programers/lv2/defence_game.js
@@ -1,5 +1,63 @@
 //디펜스 게임
+// 시도 2
+function solution(n, k, enemy) {
+  let ans = 0,
+    l = enemy.length,
+    marr = [];
 
+  function insert(x) {
+    let n = marr.length;
+    if (n > 0) {
+      let [a, b] = [0, n - 1];
+      if (x >= marr[a]) {
+        marr = [x].concat(marr);
+      } else if (x <= marr[b]) {
+        marr.push(x);
+      } else {
+        while (a < b) {
+          let m = Math.floor((a + b) * 0.5);
+          if (x > marr[m]) {
+            b = m;
+          } else {
+            a = m + 1;
+          }
+        }
+        marr = marr.slice(0, a).concat(x, marr.slice(a));
+      }
+    } else {
+      marr.push(x);
+    }
+  }
+  function get() {
+    let x = marr[0];
+    marr = marr.slice(1);
+    return x;
+  }
+
+  for (let i of enemy) {
+    if (n > i) {
+      n -= i;
+      insert(i);
+    } else if (k > 0) {
+      if (marr.length > 0) {
+        let p = get();
+        insert(i);
+        n += p - i;
+      }
+      k--;
+    } else {
+      n -= i;
+    }
+
+    if (n >= 0) {
+      ans++;
+    }
+  }
+
+  return ans;
+}
+
+// 시도 1
 function solution(n, k, enemy) {
   let ans = 0,
     l = enemy.length;
